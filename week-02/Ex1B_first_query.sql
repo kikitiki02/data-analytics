ff2494c5c858e5c361e2c9788f24c814086b277d
kikitiki02 <kikimarsh.02@gmail.com>
2026-04-12 22:49:19 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 I think sakila is a database thingy for maybe actors and movies I think?
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrived [200] records
+
+SELECT * FROM film; -- Retrived [1000] records
