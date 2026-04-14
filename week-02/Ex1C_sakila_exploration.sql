c6abd2c6fa6d2b175059789046c3dfe98651a4af
kikitiki02 <kikimarsh.02@gmail.com>
2026-04-13 22:29:08 -0500

    Exercise 1.C complete

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,11 @@
+/*
+a) The information in the actor table is like personal basic info about the actors.
+b) The information shown about films is like the title, description, release year, etc...
+c) Film category shows film Id and the views section also has actor ID
+d) It shows when the movies were rented and like what time and what staff rented it to a customer and customer ID
+e) The inventory table shows the inventory id, the store id, film id and the lastest update on the rental
+f) I feel like the film table would be best to learn the names of films because it has the names. The actor table is important too cause it shows the actors basic info in the film you might be looking for
+*/
+
+SELECT * FROM actor;
+SELECT * FROM film;
