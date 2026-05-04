101e7cebb165fd9af206cbede9b69c31641b3229
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-04 17:57:25 -0500

    Dollar practice

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,19 @@
-print('Hello world!')
+print("Hello, World!")
+
+message = "Hello, World!"
+print(message)
+
+# Hello world is printed twice because both commands said print in both strings.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+cents = cents + .25
+print(dollars + cents)
+
+d_str = str(dollars)
+c_str = str(cents)
+print("I have " + d_str + " dollars and " + c_str + " cents.")
+
