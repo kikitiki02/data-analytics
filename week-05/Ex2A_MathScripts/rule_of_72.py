4c10770b63c2ef5773f2d665e74a2e587ef34c4c
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-07 15:03:39 -0500

    added rule 72

---
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -0,0 +1,11 @@
+# Your current savings is [number].
+# A [number]% interest rate, your savings account will be
+# worth [number] in [number] years
+
+savings = 4000
+interest = .60
+years = 18
+future = savings * interest * years
+
+print(f"Your current savings is {savings}.")
+print(f"A {interest}%  interest rate, your savings account will be worth {future} in {years} years. ")
