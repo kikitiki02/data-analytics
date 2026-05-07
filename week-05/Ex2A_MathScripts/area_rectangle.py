38dc3aba6e28c096a193be35261a02c102a70685
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-07 15:45:22 -0500

    added input

--- week-05/Ex2A_MathScripts/area_rectangle.py
+++ week-05/Ex2A_MathScripts/area_rectangle.py
@@ -1,5 +1,8 @@
-side_a = 5
-side_b = 20
+side_a = float(input ("what length is side A?"))
+side_b = float(input ("what length is side B?"))
 
 area = side_a * side_b
-print(area)
+#what length is side A
+#what length is side B
+
+print(f"The area is {area}")
