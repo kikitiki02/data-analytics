c2e2f6b332f8db0c98b62888ab16a98149b1927a
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-08 12:20:03 -0500

    added taxes

---
+++ week-05/Ex2B_MoreMathScripts/taxes.py
@@ -0,0 +1,8 @@
+# Federal taxes are 23% of your salary every month. You make X amount of money
+# How much is withheld for taxes
+
+Salary = float(input("what is your montly salary?"))
+Taxes = (Salary * 0.23)
+
+
+print(f"The amount witheld is {Taxes:.2f}")
