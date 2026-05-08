3e0c900cf24f2c224ea1750e8a0457b132f05842
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-08 16:14:11 -0500

    Added distance

---
+++ week-05/Ex2B_MoreMathScripts/distance.py
@@ -0,0 +1,9 @@
+# How do you calculate the distance between coordinates (x1, y1) and (x2, y2)?
+import math
+x1 = 2
+x2 = 7
+y1 = 9
+y2 = 4
+
+distance = math.sqrt((x2 -x1)**2 + (y2 - y1)**2)
+print(f"The distance is {distance}")
