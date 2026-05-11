ef62270ab97742e8f6a171e2db2f98e1e1b3798f
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-11 10:15:10 -0500

    added loop love

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,12 @@
+# affrimation words 
+# awesome
+# great
+# fantastic
+# amazing
+
+yay_words = ["awesome","great","fantastic","amazing"]
+
+for word in yay_words:
+    print(f"Loops are {word}!")
+
+print("I <3 loops!")
