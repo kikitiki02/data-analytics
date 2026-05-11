d8f43cab59109c32d5df997b4e3a5e70941dc1e7
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-11 01:53:53 -0500

    added movie list

---
+++ week-05/Ex3B_SampleCollections/movie_list.py
@@ -0,0 +1,21 @@
+# favorite movies
+# 1. Avatar (pandora)
+# 2. Roll Bounce
+# 3. Ponyo
+# 4. Jjk Hidden Inventory
+# 5. Sinners
+
+Top_5 = ["Avatar","Roll Bounce","Ponyo","Jjk Hidden Inventory","Sinners"]
+
+print(f"My list for my top {len(Top_5)}")
+print(Top_5)
+
+print(sorted(Top_5))
+print(Top_5)
+
+Top_5.sort()
+print(Top_5)
+
+Top_5.append("The Conjuring")
+print(f"My list for my top {len(Top_5)}")
+print(sorted(Top_5))
