4cb9ae5ffa19f7557571efcc1bc1fd33f1cd30c1
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-14 15:22:29 -0500

    added quick multiply

---
+++ week-06/Ex2D_Lambdas&Generators/quick_multiplier.py
@@ -0,0 +1,15 @@
+doubler = lambda n: n * 2
+
+#8
+#-4
+# 'banana'
+
+print(doubler(8))
+print(doubler(-4))
+print(doubler('banana'))
+
+trippler = lambda n: n * 3
+print(trippler(8))
+print(trippler(-4))
+print(trippler('banana'))
+
