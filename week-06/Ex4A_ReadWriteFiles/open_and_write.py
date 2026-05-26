37bbbfd673dd2c173d7bc341849409f31a18b3b3
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-25 19:22:50 -0500

    updates

--- week-06/Ex4A_ReadWriteFiles/open_and_write.py
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -1,1 +1,15 @@
 f = open("about_me.txt", "a")
+f.write(f" a) If you could do anything for your perfect night out, where would you go and what would you do?")
+f.close()
+
+f = open("about_me.txt","r")
+# print(f.read(50)) # the () in read adds more characters it readsbased on the number put
+f.close()
+
+f = open("about_me.txt","r")
+# print(f.readline(10))
+# print(f.readline())
+
+# for i in range(1,5):
+    # print(f.readline())
+
